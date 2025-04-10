26bbd09df2d889287445026fd5409845761c84c1
AdhiKrisna <krisnahmbtn@gmail.com>
2025-04-10 18:49:07 +0700

    Update: testing trigger with config db

--- BE/config/database.js
+++ BE/config/database.js
@@ -2,10 +2,15 @@ import {Sequelize} from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const api_host = process.env.MYSQL_INSTANCE_HOST;
-const api_password = process.env.MYSQL_INSTANCE_PASS;
-const api_username = process.env.MYSQL_INSTANCE_USERNAME;
-const api_database = process.env.MYSQL_INSTANCE_DB;
+// const api_host = process.env.MYSQL_INSTANCE_HOST;
+// const api_password = process.env.MYSQL_INSTANCE_PASS;
+// const api_username = process.env.MYSQL_INSTANCE_USERNAME;
+// const api_database = process.env.MYSQL_INSTANCE_DB;
+
+const api_host = "34.27.98.41"
+const api_password = ""
+const api_username = "root"
+const api_database = "notes_tcc"
 
 const db_instance = new Sequelize(api_database, api_username, api_password, {
     host: api_host,
