01690094b097f6f4b86773a4a247aba2e6352bc0
AdhiKrisna <krisnahmbtn@gmail.com>
2025-04-10 22:17:05 +0700

    update: vite permission

--- aku-notes-fe/vite.config.js
+++ aku-notes-fe/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['tcc-fe-task6-582441420598.us-central1.run.app']
+  }
 })
