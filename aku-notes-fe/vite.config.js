3b7e37471d27ad34016bb83918ff1e5d7c2cd907
AdhiKrisna <krisnahmbtn@gmail.com>
2025-04-14 01:32:39 +0700

    a

--- aku-notes-fe/vite.config.js
+++ aku-notes-fe/vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  server: {
-    allowedHosts: ['tcc-fe-task6-582441420598.us-central1.run.app']
-  }
+  // server: {
+  //   allowedHosts: ['tcc-fe-task6-582441420598.us-central1.run.app']
+  // }
 })
